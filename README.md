f01e62f24e9d4eae9a5414e127df99aa16800c9a
antoluduenabereziuk21 <antolube20@gmail.com>
2023-08-22 16:54:55 -0300

    implementado fetch a api ok

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ APIs:
 
     Imagen random: https://cataas.com/cat/says/hello
 
+   -endpoint para usar: `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+
     Recupera un hecho aleatorio de gatos de la primera API
 
     Recuperar la primera palabra del hecho
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+
+//const CAT_ENDPOINT_IMAGE_URL=`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`;
+const CAT_ENDPOINT__RAMDOM_FACT=`https://catfact.ninja/fact`;
 export function App(){
+
+    const [fact, setFact] = useState();
+
+    useEffect(() => {
+    
+    
+      return () => {
+        fetch(CAT_ENDPOINT__RAMDOM_FACT)
+        .then(res => res.json())
+        .then(data=>{
+            const {fact} = data
+            setFact(fact)
+
+            const firstWord = fact.split(' ')[0];
+            //las tres primeras palabras
+            const threeWords = fact.split(' ').slice(0, 3).join(' ');
+            console.log(threeWords);
+            }
+        )}
+    }, [])
+    
     return(
-        <h1>App de Gatitos</h1>
-    )
+        <main>
+            <h1>App de Gatitos</h1>
+            {fact && <p>{fact}</p>}
+        </main>
+            
+        
+        
+    );
 };
