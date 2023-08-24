6381f392220e4d72365cfc9dfb53ed9c9fdd7884
antoluduenabereziuk21 <antolube20@gmail.com>
2023-08-24 10:57:37 -0300

    added img for url , and added styles in app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,50 @@
 import { useEffect, useState } from "react";
-
+import "./App.css";
 //const CAT_ENDPOINT_IMAGE_URL=`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`;
-const CAT_ENDPOINT__RAMDOM_FACT=`https://catfact.ninja/fact`;
-export function App(){
+const CAT_ENDPOINT__RAMDOM_FACT = `https://catfact.ninja/fact`;
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
+export function App() {
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
 
-    const [fact, setFact] = useState();
+  useEffect(() => {
+    return () => {
+      fetch(CAT_ENDPOINT__RAMDOM_FACT)
+        .then((res) => res.json())
+        .then((data) => {
+          const { fact } = data;
+          setFact(fact);
 
-    useEffect(() => {
-    
-    
-      return () => {
-        fetch(CAT_ENDPOINT__RAMDOM_FACT)
-        .then(res => res.json())
-        .then(data=>{
-            const {fact} = data
-            setFact(fact)
+          //const firstWord = fact.split(' ')[0];
+          //las tres primeras palabras
+          const threeFirstWord = fact.split(" ").slice(0, 3).join(" ");
+          //alternativa    = fact.split('0',3).join(' ');
+          console.log(threeFirstWord);
 
-            const firstWord = fact.split(' ')[0];
-            //las tres primeras palabras
-            const threeWords = fact.split(' ').slice(0, 3).join(' ');
-            console.log(threeWords);
-            }
+          fetch(
+            `https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`
+          )
+            .then((res) => res.json())
+            .then((response) => {
+              const { url } = response;
+              setImageUrl(url);
+            });
+        });
+    };
+  }, []);
+
+  return (
+    <main>
+      <h1>App de Gatitos</h1>
+      <section>
+        {fact && <p>{fact}</p>}
+        {imageUrl && (
+          <img
+            src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+            alt={`Image extracted using the first three words for ${fact}`}
+          />
         )}
-    }, [])
-    
-    return(
-        <main>
-            <h1>App de Gatitos</h1>
-            {fact && <p>{fact}</p>}
-        </main>
-            
-        
-        
-    );
-};
+      </section>
+    </main>
+  );
+}
